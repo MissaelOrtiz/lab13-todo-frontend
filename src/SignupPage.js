78cd39e40b552f6f68f6e079540e06edc96ea76f
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-30 16:22:36 -0700

    added handle submit, email change, and password change methods

--- src/SignupPage.js
+++ src/SignupPage.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react'
 import { signup } from './utils'
 
 export default class SignupPage extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit =  async (e) => {
+        e.preventDefault();
+
+        const token = await signup(this.state.email, this.state.password);
+        this.props.login(token)
+        this.props.history.push('/todos')
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({ email: e.target.value });
+    }
+
     render() {
         return (
             <div>
