889d2e3ba29a496524be4dfe4a14b9584b6d530c
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-30 18:13:56 -0700

    Finished!!!!

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,7 +5,11 @@ export default class TodoItem extends Component {
     render() {
         return (
             <div>
-                
+                <p>Task: {this.props.data.todo}</p>
+                <p>Completed: {this.props.data.completed.toString()}</p>
+                <button onClick={async () => { 
+                    await completeTodo(this.props.data.id, this.props.token)
+                    await this.props.event()}}>Done!</button>
             </div>
         )
     }
--- src/TodoListPage.js
+++ src/TodoListPage.js
@@ -26,13 +26,20 @@ export default class TodoListPage extends Component {
     handleNewTodo = (e) => {
         this.setState({ new_todo: e.target.value })
     }
-
+    
     render() {
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Task
+                        <input onChange={this.handleNewTodo}/>
+                    </label>
+                    <button>Add Todo</button>
+                </form>
                 {
                     this.state.todos.map((task, i) =>
-                        <TodoItem data={task} key={i} />
+                        <TodoItem data={task} key={i} token={this.props.token} event={this.doFetch}/>
                     )
                 }
             </div>
