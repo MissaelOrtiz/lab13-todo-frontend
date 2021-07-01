0f82b57fe21139e27444cec2c655197ffa22ca5e
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-30 17:13:53 -0700

    added working login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import { login } from './utils'
 
-export default class LoginPage extends Component {
+export default class SignupPage extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
+    handleSubmit =  async (e) => {
+        e.preventDefault();
+
+        const token = await login(this.state.email, this.state.password);
+        this.props.event(token)
+        this.props.history.push('/todos')
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({ email: e.target.value });
+    }
+
     render() {
         return (
             <div>
-                Login Page!!!
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email
+                        <input type="email" onChange={this.handleEmailChange}/>
+                    </label>
+                    <label>
+                        Password
+                        <input type="password" onChange={this.handlePasswordChange}/>
+                    </label>
+                    <button>Log In!</button>
+                </form>
             </div>
         )
     }
